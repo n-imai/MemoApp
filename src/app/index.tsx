ae48724157d02fafa0426dc561b0679ef275ba9c
n-imai <8196611+n-imai@users.noreply.github.com>
2025-04-16 08:20:36 +0900

    style list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,39 +11,33 @@ const Index = ():JSX.Element => {
         </View>
       </View>
 
-      <View>
+      <View style={styles.memoListItem}>
         <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
+        </View>
+        <View>
             <Text>X</Text>
-          </View>
         </View>
       </View>
 
-      <View>
+      <View style={styles.memoListItem}>
         <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
+        </View>
+        <View>
             <Text>X</Text>
-          </View>
         </View>
       </View>
 
-      <View>
+      <View style={styles.memoListItem}>
         <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
+        </View>
+        <View>
             <Text>X</Text>
-          </View>
         </View>
       </View>
 
@@ -80,6 +74,25 @@ const styles = StyleSheet.create({
     lineHeight: 32,
     fontWeight: 'bold',
     color: '#FFFFFF'
+  },
+  memoListItem: {
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal:19,
+    alignItems: 'center',
+    borderBottomWidth: 1.,
+    borderColor: 'rgba(0,0,0,0.15)'
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484'
   }
 })
 
