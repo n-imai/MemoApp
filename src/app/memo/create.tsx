f89673bded6c542fd0641ab5c1125c48e5a1027d
n-imai <8196611+n-imai@users.noreply.github.com>
2025-06-07 12:06:40 +0900

    feat: add Firestore integration to memo creation screen

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,20 +1,42 @@
 import { View, TextInput, StyleSheet, KeyboardAvoidingView} from "react-native"
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/icon";
+import {collection, addDoc, Timestamp} from "firebase/firestore"
+import {useState} from "react"
 
 import {router} from "expo-router"
+import { db, auth } from "../../config"
 
-const handlePress = () => {
-    router.back()
+const handlePress = async (bodyText:string) => {
+    if(auth.currentUser === null) {return}
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    addDoc(ref, {
+        bodyText,
+        updatedAt: Timestamp.fromDate(new Date()),
+    })
+        .then((docRef) => {
+            console.log("Success!", docRef.id)
+            router.back()
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
 }
 
 const Create = ():JSX.Element => {
+    const [bodyText, setBodyText] = useState('')
     return(
         <KeyboardAvoidingView behavior='height' style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value='' />
+                <TextInput
+                    multiline
+                    style={styles.input}
+                    value={bodyText}
+                    onChangeText={(text) => { setBodyText(text) }}
+                />
             </View>
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={() => handlePress(bodyText)}>
                 <Icon name='check' size={40} color="#ffffff"/>
             </CircleButton>
         </KeyboardAvoidingView>
