e9e3a507f30c52078df8b642f4ebc95d300543af
n-imai <8196611+n-imai@users.noreply.github.com>
2025-06-14 13:55:08 +0900

    feat: implement memo editing functionality with Firestore integration

--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,19 +1,52 @@
 import { View, TextInput, StyleSheet, KeyboardAvoidingView} from "react-native"
 import CircleButton from "../../components/CircleButton"
 import Icon from "../../components/icon"
-import {router} from "expo-router"
+import { router, useLocalSearchParams } from "expo-router"
+import { useState, useEffect } from "react"
+import { doc, getDoc, setDoc, Timestamp } from "firebase/firestore"
+import { auth, db } from "../../config"
 
-const handlePress = () => {
-    router.back()
+const handlePress = (id: string, bodyText: string) => {
+    if(auth.currentUser === null) {return}
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+    setDoc(ref, {
+        bodyText,
+        updatedAt: Timestamp.fromDate(new Date())
+    })
+        .then(() => {
+            router.back()
+        })
+        .catch((error: Error) => {
+            console.log(error)
+        })
 }
 
 const Edit = ():JSX.Element => {
+    const id = String(useLocalSearchParams().id)
+    const  [bodyText, setBodyText] = useState<string>('')
+    useEffect(() => {
+        if(auth.currentUser === null) { return }
+        const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+        getDoc(ref)
+            .then((docRef) => {
+                const RemoteBodyText = docRef.data()?.bodyText
+                setBodyText(RemoteBodyText)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+    console.log('edit', id)
     return(
         <KeyboardAvoidingView behavior='height' style={styles.container}>
             <View style={styles.inputContainer}>
-                <TextInput multiline style={styles.input} value={'買い物\nリスト'} />
+                <TextInput
+                    multiline style={styles.input}
+                    value={bodyText}
+                    onChangeText={(text) => { setBodyText(text) }}
+                />
             </View>
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={() => { handlePress(id,bodyText) }}>
                 <Icon name='check' size={40} color="#ffffff"/>
             </CircleButton>
         </KeyboardAvoidingView>
@@ -25,15 +58,15 @@ const styles = StyleSheet.create({
         flex: 1
     },
     inputContainer: {
-        paddingVertical: 32,
-        paddingHorizontal: 27,
         flex: 1
     },
     input: {
         flex: 1,
         textAlignVertical: 'top',
         fontSize: 16,
-        lineHeight: 24
+        lineHeight: 24,
+        paddingVertical: 32,
+        paddingHorizontal: 27
     }
 })
 
